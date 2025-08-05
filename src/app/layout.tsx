ffa647138129df123cb3e0ef4f3ab8e66dd2b7af
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2025-08-05 23:58:12 +0100

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,25 @@
 // app/layout.tsx
-import './globals.css';
-import '@rainbow-me/rainbowkit/styles.css';
+import type { Metadata } from "next";
+import "./globals.css";
+import { Providers } from "@/app/providers";
+import { Header } from "@/components/Header/Header";
 
-import { ReactNode } from 'react';
-import {
-  getDefaultConfig,
-  RainbowKitProvider,
-} from '@rainbow-me/rainbowkit';
-import {
-  WagmiConfig,
-} from 'wagmi';
-import { mainnet } from 'wagmi/chains';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+export const metadata: Metadata = {
+  title: "Airdrop dist",
+};
 
-const config = getDefaultConfig({
-  appName: 'Token-Gated Demo',
-  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-  chains: [mainnet],
-  ssr: false,
-});
-
-const queryClient = new QueryClient();
-
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
-        <WagmiConfig config={config}>
-          <QueryClientProvider client={queryClient}>
-            <RainbowKitProvider>
-              {children}
-            </RainbowKitProvider>
-          </QueryClientProvider>
-        </WagmiConfig>
+        <Providers>
+          <Header />
+          <main>{children}</main>
+        </Providers>
       </body>
     </html>
   );
