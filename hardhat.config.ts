6af8dac765b2464cb50ac3e83db3041270e99105
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2025-08-06 15:33:12 +0100

    update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -16,12 +16,23 @@ const config: HardhatUserConfig = {
       url: process.env.SEPOLIA_RPC_URL || "",
       accounts: process.env.SEPOLIA_PRIVATE_KEY ? [process.env.SEPOLIA_PRIVATE_KEY] : [],
     },
+    ganache: {
+      url: process.env.GANACHE_RPC_URL || "HTTP://127.0.0.1:7545",
+      chainId: 1337,
+      accounts: {
+        mnemonic: "quit know fire inform explain uniform page icon wrist vibrant salon other",
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 10
+      }
+    },
   },
   paths: {
     artifacts: "./src/abis",
   },
   typechain: {
     outDir: "./src/typechain-types",
+    target: "ethers-v6",
   },
 };
 
