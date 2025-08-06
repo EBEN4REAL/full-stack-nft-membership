6ca82ae32dfa333b7055bcc2877f9403772064b8
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2025-08-06 18:40:00 +0100

    remove unnecessary file

--- src/hooks/useBasicNft.ts
+++
@@ -1,7 +0,0 @@
-import { ethers } from "ethers";
-import abi from "@/abis/BasicNft.json";
-import deployed from "../deployed.json";
-
-export function useBasicNftContract(provider: ethers.Provider) {
-  return new ethers.Contract(deployed.BasicNft, abi, provider);
-}
